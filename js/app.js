d6ae376c732ec2af7f51699b035c11e8b3147b72
Marc-07 <maanraca01@gmail.com>
2023-06-09 15:15:30 -0500

    Se elimina cita desde el indexedDB

--- js/app.js
+++ js/app.js
@@ -284,9 +284,20 @@ function reiniciarObjeto() {
 
 
 function eliminarCita(id) {
-    administrarCitas.eliminarCita(id);
+    const transaction = DB.transaction(['citas'], 'readwrite');
+    const objectStore = transaction.objectStore('citas');
 
-    ui.imprimirCitas();
+    objectStore.delete(id);
+    
+    
+    transaction.oncomplete = function(){
+        console.log(`Cita ${id} Eliminada...`);
+        ui.imprimirCitas();
+    }
+
+    transaction.onerror = function(){
+        console.log('Hubo un error...')
+    }
 }
 
 function cargarEdicion(cita) {
